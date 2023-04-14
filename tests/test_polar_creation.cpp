6183472ce819e5943020f94bf8e92ced4d708c2b
Francois Rongere <francois.rongere@dice-engineering.com>
2023-04-14 18:10:21 +0200

    More complex polar generation test

--- tests/test_polar_creation.cpp
+++ tests/test_polar_creation.cpp
@@ -19,6 +19,7 @@ int main() {
   auto d1 = std::make_shared<DimensionID>("d1", "-", "d1", 0, 20);
   auto d2 = std::make_shared<DimensionID>("d2", "-", "d2", 0, 60);
   auto d3 = std::make_shared<DimensionID>("d3", "-", "d3", 0, 180);
+  auto d4 = std::make_shared<DimensionID>("d4", "-", "d4", 0, 50);
 
   /**
    * Create the dimension ID set
@@ -26,8 +27,11 @@ int main() {
    * A shared_ptr is used as several polars are likely to share this coordinate system
    * Dimensions are ordered
    */
-  DimensionIDSet<3>::IDSet dim_ID_set{d1, d2, d3};
-  auto dimension_ID_set = std::make_shared<DimensionIDSet<3>>(dim_ID_set);
+  DimensionIDSet<3>::IDSet dim_ID_set_double{d1, d2, d3};
+  auto dimension_ID_set_double = std::make_shared<DimensionIDSet<3>>(dim_ID_set_double);
+
+  DimensionIDSet<2>::IDSet dim_ID_set_int{d1, d4};
+  auto dimension_ID_set_int = std::make_shared<DimensionIDSet<2>>(dim_ID_set_int);
 
   /**
    * Create a dimension point set
@@ -37,16 +41,24 @@ int main() {
    * be built to generate a cartesian product of every dimension and generate internally a vector of dimension points
    */
 
-   // TODO: composer un DimensionPointSet<_dim> avec un array de SampledDim...
-  auto dimension_point_set = std::make_shared<DimensionPointSet<3>>(dimension_ID_set);
 
   // Create samples for the dimensions
   // FIXME: arange ne fonctionne vraiment pas comme voulu...
-  dimension_point_set->set_dimension_vector("d1", mathutils::arange<double>(0, 20, 1));
-  dimension_point_set->set_dimension_vector("d2", mathutils::arange<double>(0, 60, 5));
-  dimension_point_set->set_dimension_vector("d3", mathutils::arange<double>(0, 180, 15));
+  auto d1_vector = mathutils::arange<double>(0, 20, 1);
+  auto d2_vector = mathutils::arange<double>(0, 60, 5);
+  auto d3_vector = mathutils::arange<double>(0, 180, 15);
+  auto d4_vector = mathutils::arange<double>(0, 50, 2);
 
-  dimension_point_set->build();
+  auto dimension_point_set_var_double = std::make_shared<DimensionPointSet<3>>(dimension_ID_set_double);
+  dimension_point_set_var_double->set_dimension_vector("d1", d1_vector);
+  dimension_point_set_var_double->set_dimension_vector("d2", d2_vector);
+  dimension_point_set_var_double->set_dimension_vector("d3", d3_vector);
+  dimension_point_set_var_double->build();
+
+  auto dimension_point_set_var_int = std::make_shared<DimensionPointSet<2>>(dimension_ID_set_int);
+  dimension_point_set_var_int->set_dimension_vector("d1", d1_vector);
+  dimension_point_set_var_int->set_dimension_vector("d4", d4_vector);
+  dimension_point_set_var_int->build();
 
   /**
    * Creating attributes
@@ -74,17 +86,26 @@ int main() {
   /**
    * Create new polars using New method of polar set
    */
-  auto var_double = polar_set->New<double, 3>("var_double", "-", "double variable", type::DOUBLE, dimension_point_set);
-  auto var_int = polar_set->New<int, 3>("var_int", "-", "int variable", type::DOUBLE, dimension_point_set);
+  auto var_double = polar_set->New<double, 3>("var_double", "-", "double variable with dimensions [d1, d2, d3]",
+                                              type::DOUBLE, dimension_point_set_var_double);
+  auto var_int = polar_set->New<int, 2>("var_int", "-", "int variable with dimensions [d1, d4]",
+                                        type::INT, dimension_point_set_var_int);
 
 
-  double val = 0.;
-  for (const auto &dimension_point: *dimension_point_set) {
-    PolarPoint<double, 3> polar_point(dimension_point, val);
+  double val_double = 0.;
+  for (const auto &dimension_point: *dimension_point_set_var_double) {
+    PolarPoint<double, 3> polar_point(dimension_point, val_double);
     var_double->set_point(&polar_point);
+
+    val_double += 1.1;
+  }
+
+  int val_int = 0;
+  for (const auto &dimension_point: *dimension_point_set_var_int) {
+    PolarPoint<int, 2> polar_point(dimension_point, val_int);
     var_int->set_point(&polar_point);
 
-    val += 1.;
+    val_int += 1;
   }
 
   std::string nc_file("test_polar.nc");
