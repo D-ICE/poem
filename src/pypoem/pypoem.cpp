001c7f1833820ea48b8b9607a509018981a72653
hxdubois <hippolyte.dubois@dice-engineering.com>
2023-11-08 14:37:43 +0100

    interp and nearest interfaces

--- src/pypoem/pypoem.cpp
+++ src/pypoem/pypoem.cpp
@@ -4,11 +4,21 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "poem/poem.h"
 
 #include "PyWrapper.h"
+#include "poem/poem.h"
 
 namespace py = pybind11;
+
+template <size_t dim>
+void declare_interpolable_polar(py::module &m) {
+  using InterpPolar = poem::InterpolablePolar<dim>;
+
+  std::string pyclass_name = std::string("InterpolablePolar") +
+                             std::to_string((int)dim) + std::string("D");
+  py::class_<InterpPolar>(m, pyclass_name.c_str())
+      .def("interpolate", &InterpPolar::interp);
+}
 
 PYBIND11_MODULE(_pypoem, m) {
   m.doc() = "python bindings for poem.";
@@ -16,9 +26,22 @@ PYBIND11_MODULE(_pypoem, m) {
 
   py::class_<PyWrapper>(m, "PolarSetReader")
       .def(py::init<std::string>(), py::arg("path"))
-      .def_property_readonly("polar_set", &PyWrapper::get_set)
+      .def_property_readonly("polar_set", &PyWrapper::get_set,
+                             py::return_value_policy::reference)
       .def_property_readonly("attributes", &PyWrapper::get_attributes);
 
-  py::class_<poem::PolarSet, std::shared_ptr<poem::PolarSet>>(m, "PolarSet")
-      .def("polar_names", &poem::PolarSet::polar_names);
+  py::class_<poem::PolarSet, std::shared_ptr<poem::PolarSet>>(m, "_PolarSet")
+      .def_property_readonly("polar_names", &poem::PolarSet::polar_names)
+      .def("get", [](const poem::PolarSet &self, std::string &name) {
+        static_cast<poem::PolarBase *>(self.polar(name));
+      });
+
+  //    declare_interpolable_polar<4>(m);
+  //    declare_interpolable_polar<5>(m);
+
+  py::class_<poem::PolarBase>(m, "_Polar")
+      .def("interp4d", &poem::InterpolablePolar<4>::interp)
+      .def("interp5d", &poem::InterpolablePolar<5>::interp)
+      .def("nearest4i", &poem::Polar<int, 4>::nearest)
+      .def("nearest5i", &poem::Polar<int, 5>::nearest);
 }
