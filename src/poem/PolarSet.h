2063dbb254d24a6ad08060adb15ae91c60ae5334
Francois Rongere <francois.rongere@dice-engineering.com>
2024-06-28 03:08:20 +0200

    Cleaning

--- src/poem/PolarSet.h
+++ src/poem/PolarSet.h
@@ -97,7 +97,6 @@ namespace poem {
       return &m_mutex;
     }
 
-
     int to_netcdf(netCDF::NcGroup &group) const {
 
       for (const auto &polar: m_polars_map) {
@@ -110,7 +109,6 @@ namespace poem {
 
       return 0;
     }
-
 
     int to_netcdf(const std::string &nc_file) const {
 
@@ -136,124 +134,6 @@ namespace poem {
 
       return code;
     }
-
-//   private:
-//
-//    void copy_polar(std::shared_ptr<PolarBase> polar) {
-//      switch (polar->dim()) {
-//        case 1:
-//          return copy_polar < 1 > (polar);
-//        case 2:
-//          return copy_polar < 2 > (polar);
-//        case 3:
-//          return copy_polar < 3 > (polar);
-//        case 4:
-//          return copy_polar < 4 > (polar);
-//        case 5:
-//          return copy_polar < 5 > (polar);
-//        case 6:
-//          return copy_polar < 6 > (polar);
-//        default:
-//          spdlog::critical("Polar dimensions lower than 1 or higher than 6 are forbidden");
-//          CRITICAL_ERROR_POEM
-//      }
-//    }
-
-//    template<size_t _dim>
-//    void copy_polar(std::shared_ptr<PolarBase> polar) {
-//      auto type = polar->type();
-//      switch (type) {
-//        case type::INT:
-//          copy_polar<int, _dim>(polar);
-//          break;
-//        case type::DOUBLE:
-//          copy_polar<double, _dim>(polar);
-//          break;
-//        default:
-//          spdlog::critical("Type is not managed yet");
-//          CRITICAL_ERROR_POEM
-//      }
-//    }
-
-//    template<typename T, size_t _dim>
-//    void copy_polar(std::shared_ptr<PolarBase> polar) {
-//      NIY_POEM
-////      auto polar_ = static_cast<Polar<T, _dim> *>(polar);
-////
-////      auto polar_copy = New<T, _dim>(polar_->name(),
-////                                     polar_->unit(),
-////                                     polar_->description(),
-////                                     polar_->type(),
-////                                     polar_->dimension_point_set());
-////
-////      // Filling with values
-////      auto iter = polar_->begin();
-////      for (; iter != polar_->end(); ++iter) {
-////        PolarPoint<T, _dim> polar_point = (*iter).second;
-////        polar_copy->set_point(&polar_point);
-////      }
-////
-////      if (!polar_copy->is_filled()) { // Pourrait etre retire ?
-////        spdlog::critical("Copying polar while not filled");
-////        CRITICAL_ERROR
-////      }
-//
-//    }
-
-//    void generate_attributes_name_map() {
-//      NIY_POEM
-//      // TODO
-////      for (const auto &attribute: m_attributes) {
-////        auto name = attribute.first;
-////        if (name == "schema") continue;
-////
-////        if (m_schema.is_newest()) {
-////          // The attribute has not changed
-////          m_attributes_name_map.insert({name, name});
-////
-////        } else {
-////          // The attribute may have changed
-////          auto current_name = m_newest_schema.get_newest_attribute_name(name);
-////
-////          // Si current name est vide, on a pas trouve d'alias nulle part
-////          if (current_name.empty() && m_schema.get_attribute_schema(name).is_required()) {
-////            spdlog::critical("No schema compatibility for attribute named {}", name);
-////            CRITICAL_ERROR
-////          }
-////
-////          m_attributes_name_map.insert({current_name, name});
-////
-////        }
-////
-////      }
-//
-//    }
-
-//    void generate_polar_name_map(const std::string &polar_name) {
-////      NIY_POEM
-//
-////      if (m_schema.is_newest()) {
-////        m_polar_name_map.insert({polar_name, polar_name});
-////
-////      } else {
-////        auto newest_name = m_newest_schema.get_newest_variable_name(polar_name);
-////
-////        if (newest_name.empty() && m_schema.get_variable_schema(polar_name).is_required()) {
-////          spdlog::critical("No schema compatibility for polar named {}", polar_name);
-////          CRITICAL_ERROR_POEM
-////        }
-////
-////        if (newest_name.empty()) {
-////          spdlog::critical("Variable name from old schema has no equivalent in the newest schema");
-////          CRITICAL_ERROR_POEM
-////          // TODO: voir i on ne fait qu'un warning ?
-////        }
-////
-////        m_polar_name_map.insert({newest_name, polar_name});
-////
-////      }
-//
-//    }
 
    protected:
     Attributes m_attributes;
