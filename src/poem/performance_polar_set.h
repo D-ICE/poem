05adcedd1ba004d6445e874ca89d958fce32b9a3
Francois Rongere <francois.rongere@dice-engineering.com>
2024-12-12 13:08:44 +0100

    Change the way PolarSet are stored into PerformancePolarSet

--- src/poem/performance_polar_set.h
+++ src/poem/performance_polar_set.h
@@ -15,126 +15,147 @@
 namespace fs = std::filesystem;
 
 namespace poem {
-
   class PerformancePolarSet {
-    using PolarSetMap = std::unordered_map<std::string, std::shared_ptr<PolarSet>>;
+    using PolarSetMap = std::unordered_map<POLAR_TYPE, std::shared_ptr<PolarSet> >;
     using PolarSetIter = PolarSetMap::iterator;
     using PolarSetConstIter = PolarSetMap::const_iterator;
 
-   public:
-    explicit PerformancePolarSet(Attributes attributes) : m_attributes(std::move(attributes)) {}
-
-    std::shared_ptr<PolarSet> new_polar_set(const Attributes &attributes, POLAR_TYPE polar_type) {
-      if (exist(attributes["polar_type"])) {
-        spdlog::critical("Attempting to create a PolarSet with name {} twice", attributes["name"]);
-        CRITICAL_ERROR_POEM
-      }
-      auto polar_set = std::make_shared<PolarSet>("", attributes, polar_type);
-      m_polar_set_map.insert({attributes["polar_type"], polar_set});
-      return polar_set;
-    }
-
-    const std::string &name() const {
-      return m_attributes["name"];
-    }
-
-    void AddPolarSet(std::shared_ptr<PolarSet> polar_set) {
-      if (exist(polar_set->polar_type_str())) {
-        spdlog::critical("Attempting to add a PolarSet with name {} that is already present", polar_set->name());
-        CRITICAL_ERROR_POEM
-      }
-      m_polar_set_map.insert({polar_set->name(), polar_set});
-    }
-
-    std::vector<std::string> polar_set_list() const {
-      std::vector<std::string> list;
-      list.reserve(m_polar_set_map.size());
-      for (const auto &polar_set: m_polar_set_map) {
-        list.push_back(polar_set.first);
-      }
-      return list;
-    }
-
-    inline bool exist(const std::string &name) const {
-      return m_polar_set_map.contains(name); // C++20
-    }
-
-    inline bool exist(POLAR_TYPE polar_type) const {
-      return exist(polar_type_enum2s(polar_type));
-    }
-
-    void rename_variables(const std::unordered_map<std::string, std::string> varnames_map){
-      for (auto it = begin(); it!=end(); it++){
-        it->second->rename_variables(varnames_map);
-      }
-    }
-
-    std::shared_ptr<PolarSet> polar_set(const std::string &name) const {
-      if (!exist(name)) {
-        spdlog::critical("No PolarSet found with name {}", name);
-        CRITICAL_ERROR_POEM
-      }
-      return m_polar_set_map.at(name);
-    }
-
-    PolarSetIter begin() {
-      return m_polar_set_map.begin();
-    }
-
-    PolarSetIter end() {
-      return m_polar_set_map.end();
-    }
-
-    PolarSetConstIter begin() const {
-      return m_polar_set_map.cbegin();
-    }
-
-    PolarSetConstIter end() const {
-      return m_polar_set_map.cend();
-    }
-
-    int to_netcdf(const std::string &nc_filename, bool verbose = false) {
-
-      fs::path nc_file_path(nc_filename);
-      if (nc_file_path.is_relative()) {
-        nc_file_path = fs::current_path() / nc_file_path;
+    public:
+      explicit PerformancePolarSet(const std::string &name,
+                                   Attributes attributes) : m_name(name),
+                                                            m_attributes(std::move(attributes)) {
       }
 
-      if (verbose) spdlog::info("Writing NetCDF file {}", std::string(nc_file_path));
+      std::shared_ptr<PolarSet> new_polar_set(POLAR_TYPE, const Attributes &attributes, POLAR_TYPE polar_type) {
+        NIY_POEM
+        // if (exist(attributes["polar_type"])) {
+        //   spdlog::critical("Attempting to create a PolarSet with name {} twice", attributes["name"]);
+        //   CRITICAL_ERROR_POEM
+        // }
+        // auto polar_set = std::make_shared<PolarSet>("", attributes, polar_type);
+        // m_polar_set_map.insert({attributes["polar_type"], polar_set});
+        // return polar_set;
+      }
 
-      constexpr int nc_err = 2;
+      const std::string &name() const {
+        return m_name;
+      }
 
-      try {
+      void AddPolarSet(std::shared_ptr<PolarSet> polar_set) {
+        auto polar_type = polar_set->polar_type();
 
-        // Create the file. The replace parameter tells netCDF to overwrite
-        // this file, if it already exists.
-        netCDF::NcFile dataFile(std::string(nc_file_path), netCDF::NcFile::replace);
-
-        for (const auto &attribute: m_attributes) {
-          dataFile.putAtt(attribute.first, attribute.second);
+        if (m_polar_set_map.contains(polar_type)) {
+          spdlog::warn("PolarSet of type {} already exists in PerformancePolarSet {}. Assigning twice...",
+                       polar_type, name());
         }
 
-        for (const auto &polar_set: *this) {
-          auto group = dataFile.addGroup(polar_set.second->polar_type_str());
-          polar_set.second->to_netcdf(group);
-        }
+        m_polar_set_map.insert({polar_type, polar_set});
 
-        dataFile.close();
-
-      } catch (netCDF::exceptions::NcException &e) {
-        std::cerr << e.what() << std::endl;
-        return nc_err;
+        // NIY_POEM
+        // // if (exist(polar_set->polar_type_str())) {
+        // //   spdlog::critical("Attempting to add a PolarSet with name {} that is already present", polar_set->name());
+        // //   CRITICAL_ERROR_POEM
+        // // }
+        // // m_polar_set_map.insert({polar_set->name(), polar_set});
       }
 
-      return 0;
-    }
+      std::vector<POLAR_TYPE> polar_set_type_list() const {
+        std::vector<POLAR_TYPE> list;
+        list.reserve(m_polar_set_map.size());
+        for (const auto &polar_set: m_polar_set_map) {
+          list.push_back(polar_set.first);
+        }
+        return list;
+      }
 
-   private:
-    std::unordered_map<std::string, std::shared_ptr<PolarSet>> m_polar_set_map;
-    Attributes m_attributes;
+      std::vector<std::string> polar_set_list() const {
+        std::vector<std::string> list;
+        list.reserve(m_polar_set_map.size());
+        for (const auto &polar_set: m_polar_set_map) {
+          list.push_back(polar_type_enum2s(polar_set.first));
+        }
+        return list;
+      }
 
+      // inline bool exist(const std::string &name) const {
+      //   NIY_POEM
+      //   // return m_polar_set_map.contains(name); // C++20
+      // }
+
+      inline bool exist(POLAR_TYPE polar_type) const {
+        return m_polar_set_map.contains(polar_type);
+      }
+
+      void rename_variables(const std::unordered_map<std::string, std::string> varnames_map) {
+        for (auto it = begin(); it != end(); it++) {
+          it->second->rename_variables(varnames_map);
+        }
+      }
+
+      std::shared_ptr<PolarSet> polar_set(const std::string &name) const {
+        NIY_POEM
+        // if (!exist(name)) {
+        //   spdlog::critical("No PolarSet found with name {}", name);
+        //   CRITICAL_ERROR_POEM
+        // }
+        // return m_polar_set_map.at(name);
+      }
+
+      PolarSetIter begin() {
+        return m_polar_set_map.begin();
+      }
+
+      PolarSetIter end() {
+        return m_polar_set_map.end();
+      }
+
+      PolarSetConstIter begin() const {
+        return m_polar_set_map.cbegin();
+      }
+
+      PolarSetConstIter end() const {
+        return m_polar_set_map.cend();
+      }
+
+      int to_netcdf(const std::string &nc_filename, bool verbose = false) {
+        fs::path nc_file_path(nc_filename);
+        if (nc_file_path.is_relative()) {
+          nc_file_path = fs::current_path() / nc_file_path;
+        }
+
+        if (verbose) spdlog::info("Writing NetCDF file {}", std::string(nc_file_path));
+
+        constexpr int nc_err = 2;
+
+        try {
+          // Create the file. The replace parameter tells netCDF to overwrite
+          // this file, if it already exists.
+          netCDF::NcFile dataFile(std::string(nc_file_path), netCDF::NcFile::replace);
+
+          for (const auto &attribute: m_attributes) {
+            dataFile.putAtt(attribute.first, attribute.second);
+          }
+
+          for (const auto &polar_set: *this) {
+            auto group = dataFile.addGroup(polar_set.second->polar_type_str());
+            polar_set.second->to_netcdf(group);
+          }
+
+          dataFile.close();
+        } catch (netCDF::exceptions::NcException &e) {
+          std::cerr << e.what() << std::endl;
+          return nc_err;
+        }
+
+        return 0;
+      }
+
+    private:
+      std::string m_name;
+      std::unordered_map<POLAR_TYPE, std::shared_ptr<PolarSet> > m_polar_set_map;
+
+      Attributes m_attributes;
   };
-
 } // poem
 
 #endif // POEM_PERFORMANCEPOLARSET_H
--- src/poem/performance_polar_set_reader.h
+++ src/poem/performance_polar_set_reader.h
@@ -20,43 +20,38 @@
 namespace fs = std::filesystem;
 
 namespace poem {
-
   namespace details {
-
     template<typename T, size_t _dim>
     inline void read_polar(const netCDF::NcVar &nc_var,
-                           const std::unordered_map<std::string, std::shared_ptr<Dimension>> &dimension_map,
-                           const std::unordered_map<std::string, std::vector<double>> &dimension_values_map,
+                           const std::unordered_map<std::string, std::shared_ptr<Dimension> > &dimension_map,
+                           const std::unordered_map<std::string, std::vector<double> > &dimension_values_map,
                            std::shared_ptr<PolarSet> polar_set,
                            type::POEM_TYPES var_type) {
-
       std::string hash_name;
       for (int i = 0; i < _dim; ++i) {
         hash_name += nc_var.getDim(i).getName();
       }
 
-      std::unordered_map<std::string, std::shared_ptr<DimensionPointSetBase>> dimension_point_set_map;
-      std::shared_ptr<DimensionPointSet<_dim>> dimension_point_set;
+      std::unordered_map<std::string, std::shared_ptr<DimensionPointSetBase> > dimension_point_set_map;
+      std::shared_ptr<DimensionPointSet<_dim> > dimension_point_set;
 
       if (dimension_point_set_map.find(hash_name) != dimension_point_set_map.end()) {
         // This dimension point set is already registered, getting it from registry
-        dimension_point_set = std::dynamic_pointer_cast<DimensionPointSet<_dim>>(dimension_point_set_map.at(hash_name));
-
+        dimension_point_set = std::dynamic_pointer_cast<DimensionPointSet<
+          _dim> >(dimension_point_set_map.at(hash_name));
       } else {
-
         std::array<std::shared_ptr<Dimension>, _dim> array;
         for (int i = 0; i < _dim; ++i) {
           array.at(i) = dimension_map.at(nc_var.getDim(i).getName());
         }
-        auto dimension_grid = DimensionGrid(std::make_shared<DimensionSet<_dim>>(array));
+        auto dimension_grid = DimensionGrid(std::make_shared<DimensionSet<_dim> >(array));
         for (int i = 0; i < _dim; ++i) {
           auto dim_name = nc_var.getDim(i).getName();
           dimension_grid.set_values(dim_name, dimension_values_map.at(dim_name));
         }
 
-        dimension_point_set = std::make_shared<DimensionPointSet<_dim>>(dimension_grid);
+        dimension_point_set = std::make_shared<DimensionPointSet<_dim> >(dimension_grid);
         dimension_point_set_map.insert({hash_name, dimension_point_set});
-
       }
 
       std::string unit, description;
@@ -76,15 +71,13 @@ namespace poem {
       nc_var.getVar(values.data());
 
       polar->set_values(values);
-
     }
 
     template<size_t _dim>
     inline void read_polar(const netCDF::NcVar &nc_var,
-                           const std::unordered_map<std::string, std::shared_ptr<Dimension>> &dimension_map,
-                           const std::unordered_map<std::string, std::vector<double>> &dimension_values_map,
+                           const std::unordered_map<std::string, std::shared_ptr<Dimension> > &dimension_map,
+                           const std::unordered_map<std::string, std::vector<double> > &dimension_values_map,
                            std::shared_ptr<PolarSet> polar_set) {
-
       auto type = nc_var.getType();
       switch (type.getId()) {
         case netCDF::NcType::nc_DOUBLE:
@@ -98,14 +91,12 @@ namespace poem {
           CRITICAL_ERROR_POEM
       }
     }
-
   } // details
 
   inline void read_polar(const netCDF::NcVar &nc_var,
-                         const std::unordered_map<std::string, std::shared_ptr<Dimension>> &dimension_map,
-                         const std::unordered_map<std::string, std::vector<double>> &dimension_values_map,
+                         const std::unordered_map<std::string, std::shared_ptr<Dimension> > &dimension_map,
+                         const std::unordered_map<std::string, std::vector<double> > &dimension_values_map,
                          std::shared_ptr<PolarSet> polar_set) {
-
     size_t nbdim = nc_var.getDimCount();
 
     switch (nbdim) {
@@ -128,7 +119,6 @@ namespace poem {
   }
 
   inline std::shared_ptr<PolarSet> read_polar_set(const netCDF::NcGroup &group) {
-
     Attributes attributes;
     for (const auto &att: group.getAtts()) {
       std::string att_val;
@@ -141,8 +131,8 @@ namespace poem {
     auto polar_set = std::make_shared<PolarSet>(group.getName(), attributes, polar_type);
 
     // Get dimensions
-    std::unordered_map<std::string, std::shared_ptr<Dimension>> dimension_map;
-    std::unordered_map<std::string, std::vector<double>> dimension_values_map;
+    std::unordered_map<std::string, std::shared_ptr<Dimension> > dimension_map;
+    std::unordered_map<std::string, std::vector<double> > dimension_values_map;
     for (const auto &dim_: group.getDims()) {
       auto dim_name = dim_.first;
 
@@ -150,7 +140,7 @@ namespace poem {
 
       // TODO: on devrait plutot faire un load dynamique...
       std::string unit, description;
-      dim_var.getAtt("unit").getValues(unit);  // FIXME: devrait etre plus generique !
+      dim_var.getAtt("unit").getValues(unit); // FIXME: devrait etre plus generique !
       dim_var.getAtt("description").getValues(description);
 
       auto dim = std::make_shared<Dimension>(dim_name, unit, description);
@@ -170,7 +160,6 @@ namespace poem {
       if (dimension_map.find(var_name) != dimension_map.end()) continue;
 
       read_polar(group.getVar(var_name), dimension_map, dimension_values_map, polar_set);
-
     }
 
     return polar_set;
@@ -196,7 +185,6 @@ namespace poem {
     if (attributes.contains("poem_file_format_version")) {
       has_groups = true;
       poem_spec_version = (int) semver::version::parse(attributes.get("poem_file_format_version"), false).major();
-
     } else {
       has_groups = false;
       poem_spec_version = 0;
@@ -204,7 +192,7 @@ namespace poem {
 
     spdlog::info("Polar File follows the POEM specifications version v{}", poem_spec_version);
 
-    auto performance_polar_set = std::make_shared<PerformancePolarSet>(attributes);
+    auto performance_polar_set = std::make_shared<PerformancePolarSet>(group.getName(), attributes);
 
     // A PolarSet is a NetCDF Dataset, i.e. a group
 
@@ -213,10 +201,7 @@ namespace poem {
       // No groups in the NetCDF-4 file, only one DataSet, this is the root group
       performance_polar_set->AddPolarSet(read_polar_set(group));
       NIY_POEM
-
     } else {
-
-
       // TODO: voir si on fixe pas en dur les groupes qu'il est possible d'aller chercher sur la base de PolarType
       for (const auto &group_: group.getGroups()) {
         performance_polar_set->AddPolarSet(read_polar_set(group_.second));
@@ -227,7 +212,6 @@ namespace poem {
   }
 
   inline std::shared_ptr<PerformancePolarSet> read_performance_polar_set(const std::string &nc_polar) {
-
     // Does the file exist
     if (!fs::exists(nc_polar)) {
       spdlog::critical("Polar file {} NOT FOUND", nc_polar);
@@ -256,7 +240,6 @@ namespace poem {
   }
 
   [[nodiscard]] inline std::shared_ptr<PerformancePolarSet> read_v0(const std::string &nc_polar) {
-
     /**
      * In v0 spec, only PolarSet are store into the NetCDF file, ie only one root group
      *
@@ -271,12 +254,12 @@ namespace poem {
 
      */
 
-//    auto perf_polar_set = std::make_shared<PerformancePolarSet>();
+    //    auto perf_polar_set = std::make_shared<PerformancePolarSet>();
     netCDF::NcFile ncfile(nc_polar, netCDF::NcFile::read);
     auto polar_set = read_polar_set(ncfile);
     ncfile.close();
 
-//    R1
+    //    R1
     auto atts = polar_set->attributes();
     if (!atts.contains("file_type")) {
       atts.add_attribute("file_type", "poem");
@@ -285,14 +268,14 @@ namespace poem {
       atts.add_attribute("poem_file_format_version", "v1");
     }
 
-    auto perf_polar_set = std::make_shared<PerformancePolarSet>(atts);
+    auto perf_polar_set = std::make_shared<PerformancePolarSet>("/", atts);
 
-//    R2
+    //    R2
     polar_set->polar_type("HPPP");
 
-    auto BP_polar = std::reinterpret_pointer_cast<Polar<double, 5>>(polar_set->polar("BrakePower"));
+    auto BP_polar = std::reinterpret_pointer_cast<Polar<double, 5> >(polar_set->polar("BrakePower"));
 
-//    R3
+    //    R3
     auto ds = BP_polar->dimension_set();
     ds->at("STW_kt")->rename("STW_Coord");
     ds->at("TWS_kt")->rename("TWS_Coord");
@@ -300,18 +283,16 @@ namespace poem {
     ds->at("WA_deg")->rename("WA_Coord");
     ds->at("Hs_m")->rename("Hs_Coord");
 
-//    R6
+    //    R6
     BP_polar->rename("TotalBrakePower");
 
     perf_polar_set->AddPolarSet(polar_set);
 
     return perf_polar_set;
-
   }
 
   [[nodiscard]] inline std::shared_ptr<PerformancePolarSet> read_v1(const std::string &nc_polar) {
-
-//    auto perf_polar_set = std::make_shared<PerformancePolarSet>();
+    //    auto perf_polar_set = std::make_shared<PerformancePolarSet>();
     netCDF::NcFile ncfile(nc_polar, netCDF::NcFile::read);
 
     Attributes attributes;
@@ -320,7 +301,7 @@ namespace poem {
       att.second.getValues(att_val);
       attributes.add_attribute(att.first, att_val);
     }
-    auto perf_polar_set = std::make_shared<PerformancePolarSet>(attributes);
+    auto perf_polar_set = std::make_shared<PerformancePolarSet>("/", attributes);
 
     for (auto group: ncfile.getGroups()) {
       auto polar_set = read_polar_set(group.second);
@@ -330,60 +311,53 @@ namespace poem {
     ncfile.close();
 
     return perf_polar_set;
-
   }
 
   class Reader {
-   public:
-    explicit Reader(const std::string &nc_polar) : m_nc_polar(nc_polar) {
-
-      if (!fs::exists(nc_polar)) {
-        spdlog::critical("Polar file {} NOT FOUND", nc_polar);
-        CRITICAL_ERROR_POEM
-      }
-
-      // Get the version
-      m_poem_file_version = get_poem_file_version(nc_polar);
-
-      // Check the poem file is compliant with the version
-      auto spec_rules = SpecRules(m_poem_file_version);
-      if (!spec_rules.check(nc_polar, true)) {
-        spdlog::warn("File said to be of POEM standard version {} but not compliant: {}",
-                     m_poem_file_version, m_nc_polar);
-      }
-
-    }
-
-    int poem_file_format_version() const {
-      return m_poem_file_version;
-    }
-
-    [[nodiscard]] std::shared_ptr<PerformancePolarSet> read() const {
-
-      std::shared_ptr<PerformancePolarSet> perf_polar_set;
-      switch (m_poem_file_version) {
-        case 0: {
-          perf_polar_set = read_v0(m_nc_polar);
-          break;
-        }
-        case 1: {
-          perf_polar_set = read_v1(m_nc_polar);
-          break;
+    public:
+      explicit Reader(const std::string &nc_polar) : m_nc_polar(nc_polar) {
+        if (!fs::exists(nc_polar)) {
+          spdlog::critical("Polar file {} NOT FOUND", nc_polar);
+          CRITICAL_ERROR_POEM
         }
 
-        // TODO: add new version reader
+        // Get the version
+        m_poem_file_version = get_poem_file_version(nc_polar);
+
+        // Check the poem file is compliant with the version
+        auto spec_rules = SpecRules(m_poem_file_version);
+        if (!spec_rules.check(nc_polar, true)) {
+          spdlog::warn("File said to be of POEM standard version {} but not compliant: {}",
+                       m_poem_file_version, m_nc_polar);
+        }
       }
 
-      return perf_polar_set;
-    }
+      int poem_file_format_version() const {
+        return m_poem_file_version;
+      }
 
-   private:
-    std::string m_nc_polar;
-    int m_poem_file_version;
+      [[nodiscard]] std::shared_ptr<PerformancePolarSet> read() const {
+        std::shared_ptr<PerformancePolarSet> perf_polar_set;
+        switch (m_poem_file_version) {
+          case 0: {
+            perf_polar_set = read_v0(m_nc_polar);
+            break;
+          }
+          case 1: {
+            perf_polar_set = read_v1(m_nc_polar);
+            break;
+          }
 
+          // TODO: add new version reader
+        }
+
+        return perf_polar_set;
+      }
+
+    private:
+      std::string m_nc_polar;
+      int m_poem_file_version;
   };
-
-
 } // poem
 
 #endif // POEM_PERFORMANCEPOLARSETREADER_H
