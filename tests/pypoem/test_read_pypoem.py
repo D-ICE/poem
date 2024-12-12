9385f2d6e05171ddd2793cf96ee11302951194f2
blafoux <baptiste.lafoux@dice-engineering.com>
2024-12-12 15:59:41 +0100

    Update test_read_pypoem

--- tests/pypoem/test_read_pypoem.py
+++ tests/pypoem/test_read_pypoem.py
@@ -4,7 +4,7 @@ import os
 import boto3, botocore
 
 TEST_DATA_BUCKET_NAME = "s3://dice-test-data"
-PERF_POLARSET_PATH = "/poem/Canopee-perf-polars_20241203_18H05.nc"
+PERF_POLARSET_PATH_S3 = "/poem/Canopee-perf-polars_20241203_18H05.nc"
 
 
 @pytest.fixture
@@ -14,10 +14,13 @@ def random() -> None:
 
 def download_test_data_from_s3(
     bucket_name: str = TEST_DATA_BUCKET_NAME,
-    file_path: str = PERF_POLARSET_PATH,
+    file_path_s3: str = PERF_POLARSET_PATH_S3,
 ) -> None:
     try:
-        boto3.resource("s3").download_file(bucket_name, file_path, "my_local_image.jpg")
+        boto3.resource("s3").download_file(
+            bucket_name,
+            file_path_s3,
+        )
     except botocore.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "404":
             print("The test data you are looking for does not exist.")
