aabd59814262db9261ee04dd9e60e8f51b117e60
blafoux <baptiste.lafoux@dice-engineering.com>
2024-12-13 17:17:41 +0100

    Add test_read_pypoem.py

--- tests/pypoem/test_read_poem.py
+++ tests/pypoem/test_read_poem.py
@@ -1,103 +1,103 @@
-# import poem
-import os
-import logging
-# import numpy as np
+# # import poem
+# import os
+# import logging
+# # import numpy as np
 
-import pypoem
-# logging.basicConfig(level=logging.info)
+# import pypoem
+# # logging.basicConfig(level=logging.info)
 
-""" FOR TOMORROW => MAKE THE FIX FOR SHARED_PTR OBJECT , shall be direct with *"""
-def test_poem():
-    pass
-# "src/pypoem/poem.cpython-310-x86_64-linux-gnu.so"
-POEM_RESOURCE_DIR= '/home/smichel/softwares/poem_gitlab/build/resources'
-polar_file = os.path.join(POEM_RESOURCE_DIR, "poem_v1_example_5_polars.nc")
-# polar_file = os.path.join(POEM_RESOURCE_DIR, "poem_v1_example.nc")
+# """ FOR TOMORROW => MAKE THE FIX FOR SHARED_PTR OBJECT , shall be direct with *"""
+# def test_poem():
+#     pass
+# # "src/pypoem/poem.cpython-310-x86_64-linux-gnu.so"
+# POEM_RESOURCE_DIR= '/home/smichel/softwares/poem_gitlab/build/resources'
+# polar_file = os.path.join(POEM_RESOURCE_DIR, "poem_v1_example_5_polars.nc")
+# # polar_file = os.path.join(POEM_RESOURCE_DIR, "poem_v1_example.nc")
 
-PerfPolarSet = pypoem.read_performance_polar_set(polar_file)
+# PerfPolarSet = pypoem.read_performance_polar_set(polar_file)
 
-print("====================== LET'S START test_read_poem.cpp")
-print("Hola, this unit test load and read everything it can from {}:".format(polar_file))
+# print("====================== LET'S START test_read_poem.cpp")
+# print("Hola, this unit test load and read everything it can from {}:".format(polar_file))
 
-print("Attribute name: {}".format(PerfPolarSet.name()))
+# print("Attribute name: {}".format(PerfPolarSet.name()))
 
-#   // smichel: pb, need to be fixed
-#   all_polar_type_list = {PPP, HPPP, HVPP, MVPP, VPP};
-#   for (auto pl : all_polar_type_list)
-#   {
-#     auto name = polar_type_enum2s(pl);
-#     std::cout << "Does my PerfPolarSet contains " << name << " ? => ";
-#     std::cout << pps_ptr->exist(pl) << std::endl;
-#   }
+# #   // smichel: pb, need to be fixed
+# #   all_polar_type_list = {PPP, HPPP, HVPP, MVPP, VPP};
+# #   for (auto pl : all_polar_type_list)
+# #   {
+# #     auto name = polar_type_enum2s(pl);
+# #     std::cout << "Does my PerfPolarSet contains " << name << " ? => ";
+# #     std::cout << pps_ptr->exist(pl) << std::endl;
+# #   }
 
-polar_set_list = PerfPolarSet.polar_set_list()
-print( "Polar set list (names): {}".format(polar_set_list))
+# polar_set_list = PerfPolarSet.polar_set_list()
+# print( "Polar set list (names): {}".format(polar_set_list))
 
-print("\n\n======================")
-print("For each PolarSet, return info ")
-print("======================")
+# print("\n\n======================")
+# print("For each PolarSet, return info ")
+# print("======================")
 
-print("Construction of PolarSet from the polar_set function")
-for p in polar_set_list:
-    print("Setting of {} as a PolarSet".format(p))
-    PolarSet = PerfPolarSet.polar_set(p)
-    print("Setting of {} as a PolarSet - DONE".format(p))
+# print("Construction of PolarSet from the polar_set function")
+# for p in polar_set_list:
+#     print("Setting of {} as a PolarSet".format(p))
+#     PolarSet = PerfPolarSet.polar_set(p)
+#     print("Setting of {} as a PolarSet - DONE".format(p))
 
-    print("- PolarSet name: {}".format(PolarSet.name()))
-    print("  PolarSet type: {}".format(PolarSet.polar_type_str()))
-    polar_names=PolarSet.polar_names()
-    print("  PolarSet contains: {}".format(polar_names))
-    if PolarSet.polar_type_str() == 'VPP':
-        dimension_point = [10.1, 90.1, 0, 0]
-        print("  Dimension point: {}".format(dimension_point))
-        #
-        for pln in polar_names:
-            print("\n  Creation of the InterpolableTable ({}) ".format(pln))
-            my4dpolar = PolarSet.get_4D_polar(pln)
-            print("  Creation of the InterpolableTable ({}) - DONE ".format(pln))
-            print("  Polar dim: {}".format(my4dpolar.dim()))
-            # print("  Polar type: {}".format(my5dpolar.polar_type())) // polar type object needed > but useless, the info is already in the PolarSet
-            print("  Polar size: {}".format(my4dpolar.size()))
-            print("  Polar is filled: {}".format(my4dpolar.is_filled()))
-            dim_names = my4dpolar.dimension_set_names()
-            for dn in dim_names:
-                print("  Dimension {}: min={}, max={}".format(dn, my4dpolar.min_bounds(dn), my4dpolar.max_bounds(dn)))
-                print("  {}".format(my4dpolar.coordinates(dn)))
-        
-            print("    Min & max values of {}: min={}, max={}".format(p,my4dpolar.min_value(),my4dpolar.max_value()))
-            nr = my4dpolar.nearest(dimension_point,True)
-            print("  Nearest: {}".format(nr))
-            interp = my4dpolar.interp(dimension_point,True)
-            print("  Interpolation: {}".format(interp))
-    
-    else:
-        if PolarSet.polar_type_str() == 'HVPP' or PolarSet.polar_type_str() == 'MVPP':
-            dimension_point = [3000, 10.1, 90.1, 0, 0]
-        else:
-            dimension_point = [10.1, 10.1, 90.1, 0, 0]
-        print("  Dimension point: {}".format(dimension_point))
-        #
-        for pln in polar_names:
-            print("\n  Creation of the InterpolableTable ({}) ".format(pln))
-            my5dpolar = PolarSet.get_5D_polar(pln)
-            print("  Creation of the InterpolableTable ({}) - DONE ".format(pln))
-            print("  Polar dim: {}".format(my5dpolar.dim()))
-            # print("  Polar type: {}".format(my5dpolar.polar_type())) // polar type object needed > but useless, the info is already in the PolarSet
-            print("  Polar size: {}".format(my5dpolar.size()))
-            print("  Polar is filled: {}".format(my5dpolar.is_filled()))
-            dim_names = my5dpolar.dimension_set_names()
-            for dn in dim_names:
-                print("  Dimension {}: min={}, max={}".format(dn, my5dpolar.min_bounds(dn), my5dpolar.max_bounds(dn)))
-                print("  {}".format(my5dpolar.coordinates(dn)))
-        
-            print("    Min & max values of {}: min={}, max={}".format(p,my5dpolar.min_value(),my5dpolar.max_value()))
-    #       // auto dpl = polar->dimension_point_list();
-    #       // for (auto dp: dim_set){
-    #       //   std::cout << dp << std::endl;
-    #       // }
-            nr = my5dpolar.nearest(dimension_point,True)
-            print("  Nearest: {}".format(nr))
-            interp = my5dpolar.interp(dimension_point,True)
-            print("  Interpolation: {}".format(interp))
+#     print("- PolarSet name: {}".format(PolarSet.name()))
+#     print("  PolarSet type: {}".format(PolarSet.polar_type_str()))
+#     polar_names=PolarSet.polar_names()
+#     print("  PolarSet contains: {}".format(polar_names))
+#     if PolarSet.polar_type_str() == 'VPP':
+#         dimension_point = [10.1, 90.1, 0, 0]
+#         print("  Dimension point: {}".format(dimension_point))
+#         #
+#         for pln in polar_names:
+#             print("\n  Creation of the InterpolableTable ({}) ".format(pln))
+#             my4dpolar = PolarSet.get_4D_polar(pln)
+#             print("  Creation of the InterpolableTable ({}) - DONE ".format(pln))
+#             print("  Polar dim: {}".format(my4dpolar.dim()))
+#             # print("  Polar type: {}".format(my5dpolar.polar_type())) // polar type object needed > but useless, the info is already in the PolarSet
+#             print("  Polar size: {}".format(my4dpolar.size()))
+#             print("  Polar is filled: {}".format(my4dpolar.is_filled()))
+#             dim_names = my4dpolar.dimension_set_names()
+#             for dn in dim_names:
+#                 print("  Dimension {}: min={}, max={}".format(dn, my4dpolar.min_bounds(dn), my4dpolar.max_bounds(dn)))
+#                 print("  {}".format(my4dpolar.coordinates(dn)))
 
-print("\n====================== END OF THE TEST")
+#             print("    Min & max values of {}: min={}, max={}".format(p,my4dpolar.min_value(),my4dpolar.max_value()))
+#             nr = my4dpolar.nearest(dimension_point,True)
+#             print("  Nearest: {}".format(nr))
+#             interp = my4dpolar.interp(dimension_point,True)
+#             print("  Interpolation: {}".format(interp))
+
+#     else:
+#         if PolarSet.polar_type_str() == 'HVPP' or PolarSet.polar_type_str() == 'MVPP':
+#             dimension_point = [3000, 10.1, 90.1, 0, 0]
+#         else:
+#             dimension_point = [10.1, 10.1, 90.1, 0, 0]
+#         print("  Dimension point: {}".format(dimension_point))
+#         #
+#         for pln in polar_names:
+#             print("\n  Creation of the InterpolableTable ({}) ".format(pln))
+#             my5dpolar = PolarSet.get_5D_polar(pln)
+#             print("  Creation of the InterpolableTable ({}) - DONE ".format(pln))
+#             print("  Polar dim: {}".format(my5dpolar.dim()))
+#             # print("  Polar type: {}".format(my5dpolar.polar_type())) // polar type object needed > but useless, the info is already in the PolarSet
+#             print("  Polar size: {}".format(my5dpolar.size()))
+#             print("  Polar is filled: {}".format(my5dpolar.is_filled()))
+#             dim_names = my5dpolar.dimension_set_names()
+#             for dn in dim_names:
+#                 print("  Dimension {}: min={}, max={}".format(dn, my5dpolar.min_bounds(dn), my5dpolar.max_bounds(dn)))
+#                 print("  {}".format(my5dpolar.coordinates(dn)))
+
+#             print("    Min & max values of {}: min={}, max={}".format(p,my5dpolar.min_value(),my5dpolar.max_value()))
+#     #       // auto dpl = polar->dimension_point_list();
+#     #       // for (auto dp: dim_set){
+#     #       //   std::cout << dp << std::endl;
+#     #       // }
+#             nr = my5dpolar.nearest(dimension_point,True)
+#             print("  Nearest: {}".format(nr))
+#             interp = my5dpolar.interp(dimension_point,True)
+#             print("  Interpolation: {}".format(interp))
+
+# print("\n====================== END OF THE TEST")
--- tests/pypoem/test_read_pypoem.py
+++ tests/pypoem/test_read_pypoem.py
@@ -1,39 +1,29 @@
 import pytest
+import logging
 import pypoem
-import os
-import boto3, botocore
+from test_tools import download_test_data_from_s3
 
-TEST_DATA_BUCKET_NAME = "s3://dice-test-data"
-PERF_POLARSET_PATH_S3 = "/poem/Canopee-perf-polars_20241203_18H05.nc"
+TEST_DATA_BUCKET_NAME = "dice-test-data"
+PERF_POLARSET_NAME = "Canopee-perf-polars_20241203_18H05.nc"
+TEST_DATA_DIR_S3 = "poem"
+TEST_DATA_DIR_LOCAL = "tmp"
 
 
 @pytest.fixture
-def random() -> None:
-    pass
+def perf_polar_set():
+    file_path = download_test_data_from_s3(
+        bucket_name=TEST_DATA_BUCKET_NAME,
+        file_name=PERF_POLARSET_NAME,
+        dir_s3=TEST_DATA_DIR_S3,
+        dir_local=TEST_DATA_DIR_LOCAL,
+    )
+    return pypoem.read_performance_polar_set(file_path)
 
 
-def download_test_data_from_s3(
-    bucket_name: str = TEST_DATA_BUCKET_NAME,
-    file_path_s3: str = PERF_POLARSET_PATH_S3,
-) -> None:
-    try:
-        boto3.resource("s3").download_file(
-            bucket_name,
-            file_path_s3,
-        )
-    except botocore.exceptions.ClientError as e:
-        if e.response["Error"]["Code"] == "404":
-            print("The test data you are looking for does not exist.")
-        else:
-            raise
-
-
-def test_load_PerformancePolarSet() -> None:
-
-    TEST_DATA_BUCKET.download_file(".", PERF_POLARSET_PATH)
-
-    perf_polar_set = pypoem.read_performance_polar_set(polar_file)
+def test_perf_polar_set(perf_polar_set):
+    """Test that we can load the performance polar set."""
+    assert perf_polar_set is not None
 
 
 if __name__ == "__main__":
-    random()
+    pytest.main([__file__])
