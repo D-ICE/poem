dcee53d6eaf3e0f3e2f687d3491bc4155b9a59ad
Francois Rongere <francois.rongere@dice-engineering.com>
2025-01-29 02:08:56 +0100

    export existing symbols from pypoem

--- src/pypoem/__init__.py
+++ src/pypoem/__init__.py
@@ -1,3 +1,22 @@
 from .pypoem import __doc__
 
-__all__ = ["__doc__"]
+__all__ = ["__doc__",
+           "current_standard_poem_version",
+           "make_dimension",
+           "make_dimension_set",
+           "DimensionPoint",
+           "DimensionGrid",
+           "make_dimension_grid",
+           "POEM_DATATYPE",
+           "PolarNode",
+           "PolarTableDouble",
+           "PolarTableInt",
+           "make_polar_table_double",
+           "make_polar_table_int",
+           "POLAR_MODE",
+           "PolarSet",
+           "make_polar_set",
+           "Polar",
+           "make_polar",
+           "to_netcdf",
+           ]
